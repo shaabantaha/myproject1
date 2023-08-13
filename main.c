6bb3c0030fb497b0b343475d58e880cff3f09243
shaaban <engshaabantaha@gmail.com>
2023-08-14 00:28:29 +0300

    creating hello message

---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "stdtypes.h"
+
+
+int main(void){
+    s_int32 c = 5;
+    printf("hello git \n");
+}
---
+++ stdtypes.h
@@ -0,0 +1,8 @@
+#ifndef _stdtypes.h_
+#define _stdtypes.h_
+
+
+#include <stdio.h>
+
+typedef int s_int32;
+#endif
